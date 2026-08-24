d9a9878dac4c6f29fb12c90d1f1c063832d2ac17
AngelaC-O <cotrinaangela507@gmail.com>
2026-08-24 01:50:42 -0500

    Implementación CRUD de categorías

---
+++ src/main/java/pe/edu/upeu/PharmaBackend/dto/CategoriaResponseDTO.java
@@ -0,0 +1,23 @@
+package pe.edu.upeu.PharmaBackend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoriaResponseDTO {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private Boolean estado;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaModificacion;
+
+}
